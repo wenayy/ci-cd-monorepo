a6abe938fe56886b5cf07170bd4874bf74f3cb1a
vinay joshi <vinay@vinay-2.local>
2025-08-17 16:39:26 +0530

    added the prisma client and monorepo things

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,23 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-
+import {client} from "@repo/db/client";
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
   srcDark: string;
 };
 
+
 const ThemeImage = (props: Props) => {
   const { srcLight, srcDark, ...rest } = props;
-
+client.user.create({
+  data: {
+    name: "Vinay Joshi"
+  }
+})
   return (
     <>
+    
       <Image {...rest} src={srcLight} className="imgLight" />
       <Image {...rest} src={srcDark} className="imgDark" />
     </>
