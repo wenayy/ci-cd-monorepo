ea466a283927ac561c84beed4f41122a246f99a8
vinay joshi <vinay@vinay-2.local>
2025-08-18 02:04:45 +0530

    nginx configurateion

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -1,14 +1,14 @@
 import { WebSocketServer }  from "ws";
 import {client} from "@repo/db/client"
 
-const server = new WebSocketServer({ port: 8080 });
+const server = new WebSocketServer({ port: 8081 });
 
 server.on("connection",async(socket)=>{
     await client.user.create({
         data:{
             name:Math.random().toString(36).substring(7,12),
             password:Math.random().toString(36).substring(7,12),
-            email:"vinayjjoshi.310@gmail.c"
+            email:Math.random().toString(36).substring(7,12)
         }
     })
     socket.send("hi there u are connected in server")
